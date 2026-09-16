ea084c061e44deceeeccf5441bf2901a3fbc48c3
Hoang <hoangutc7@gmail.com>
2026-09-16 19:47:22 +0700

    lab 2

--- learn-typescript/src/lab2.js
+++ learn-typescript/src/lab2.js
@@ -6,9 +6,45 @@ var Rate;
     Rate["medium"] = "Trung binh";
     Rate["hight"] = "Cao";
 })(Rate || (Rate = {}));
-let product = {
-    name: "Áo",
-    price: 200000,
-    sale: true,
-    rate: Rate.hight
-};
+let listProducts = [{
+        name: "Áo",
+        price: 200000,
+        sale: true,
+        rate: Rate.hight
+    },
+    {
+        name: "Quần jean",
+        price: 10,
+        sale: false,
+        rate: Rate.hight
+    },
+    {
+        name: "Giày Nike",
+        price: 20,
+        sale: true,
+        rate: Rate.hight
+    },
+    {
+        name: "Mũ lưỡi trai",
+        price: 3,
+        sale: true,
+        rate: Rate.low
+    },
+    {
+        name: "Balo",
+        price: 8,
+        sale: false,
+        rate: Rate.medium
+    }
+];
+function addDescription() {
+    let newList = listProducts.map((product) => {
+        return {
+            ...product,
+            description: product.price > 5 ? "Tốt" : "Bình thường"
+        };
+    });
+    return newList;
+}
+console.log("Danh sách sau khi thêm description:");
+console.log(addDescription());
